3d320ccddad3808adc80bdb3710e67e9c08dd1e7
Alex <alex@jetweels.com>
2024-01-02 23:39:47 +0700

    test: pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { useState } from "react";
-import "./App.css";
-import bananas from "./Bananas.svg";
+import { useEffect, useState } from 'react';
+import './App.css';
+import bananas from './Bananas.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isPwa, setIsPwa] = useState(false);
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia('(display-mode: standalone)');
+    matchMedia.addEventListener('change', (e) => {
+      setIsPwa(e.matches);
+    });
+    setIsPwa(matchMedia.matches); // Set initial state
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <div>
-        <a href="#">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href='#'>
+          <img src='/vite.svg' className='logo' alt='Vite logo' />
         </a>
-        <a href="#">
-          <img src={bananas} className="logo react" alt="React logo" />
+        <a href='#'>
+          <img src={bananas} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <h3>
-          PWA Boilerplate
-        </h3>
+        <h3>PWA Boilerplate</h3>
         <p>Cache all the things!!</p>
+
+        <p>{isPwa ? 'PWA' : 'Browser'}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
