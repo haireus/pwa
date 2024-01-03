2ae7fcdb7c2aa3bbbb8cc88b33ca33624ee69050
Alex <alex@jetweels.com>
2024-01-03 09:48:47 +0700

    fix pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,42 +8,47 @@ export default defineConfig({
     react(),
     VitePWA({
       workbox: {
-        globPatterns: ["**/*"],
+        globPatterns: ['**/*'],
       },
-      includeAssets: [
-        "**/*",
-      ],
+      includeAssets: ['**/*'],
       manifest: {
-        "theme_color": "#f69435",
-        "background_color": "#f69435",
-        "display": "standalone",
-        "scope": "/",
-        "start_url": "/",
-        "short_name": "Vite PWA",
-        "description": "Vite PWA Boilerplate",
-        "name": "Vite PWA Boilerplate",
-        "icons": [
-            {
-                "src": "/icon-192x192.png",
-                "sizes": "192x192",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-256x256.png",
-                "sizes": "256x256",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-384x384.png",
-                "sizes": "384x384",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-512x512.png",
-                "sizes": "512x512",
-                "type": "image/png"
-            }
-        ]
+        theme_color: '#f69435',
+        background_color: '#f69435',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        short_name: 'Vite PWA',
+        description: 'Vite PWA Boilerplate',
+        name: 'Vite PWA Boilerplate',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        related_applications: [
+          {
+            platform: 'android',
+            id: '/',
+            url: '/',
+          },
+        ],
       },
     }),
   ],
